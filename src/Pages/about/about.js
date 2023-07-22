40028f0f9d1a642a5ce3f062d9cfd997ea98a0c3
Kunjan <kunjan@controlshift.in>
2023-07-23 04:22:59 +0530

    added about desc

--- src/Pages/about/about.js
+++ src/Pages/about/about.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./about.css";
+import womenImgForAboutSection from "asset/img/woman-g5e97a2e26_1280.png"
 export default function About() {
   return (
     <div className="homepage-section-container">
       <div className="main-title">About Myself</div>
+    
       <div className="summary-text-container">
         <div className="summary">
-          Passionate frontend engineer with expertise in ReactJS, HTML, CSS, and
+          {/* Passionate frontend engineer with expertise in ReactJS, HTML, CSS, and
           JavaScript. 2 years of experience delivering exceptional results by
           collaborating with clients and leading development teams. Well-rounded
           education in Commerce. Constantly updating skills through online
@@ -14,8 +16,41 @@ export default function About() {
           problem-solving. Proficient in creating user-friendly applications
           with intuitive UI components and efficient business logic. Strong
           communication and collaboration skills. Let's connect to discuss how I
-          can contribute to your project's success
+          can contribute to your project's success */}
+          <ul>
+            <li>
+              Passionate frontend engineer with expertise in ReactJS, HTML, CSS,
+              and JavaScript.
+            </li>
+            <li>
+              2 years of experience delivering exceptional results through
+              collaboration with clients and leading development teams.
+            </li>
+            <li>
+              Well-rounded education in Commerce, providing a unique perspective
+              on user behavior and business requirements.
+            </li>
+            <li>
+              Constantly updating skills through online courses to stay at the
+              cutting edge of technology.
+            </li>
+            <li>
+              Creative problem-solver, crafting user-friendly applications with
+              intuitive UI components and efficient business logic.
+            </li>
+            <li>
+              Strong communication and collaboration skills to foster effective
+              teamwork.
+            </li>
+            <li>
+              Diverse portfolio showcasing attention to detail and innovative
+              solutions.
+            </li>
+          </ul>
         </div>
+  
+        <img src={womenImgForAboutSection} className="women-img"></img>
+ 
       </div>
     </div>
   );
